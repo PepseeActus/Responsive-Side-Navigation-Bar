ad0f193f5a64d49005faad7af7385be8ba59d319
PepseeActus <christophe.sbastien@gmail.com>
2021-09-28 23:38:56 +0200

    New responsive sidebar for PepseeActus

---
+++ app.js
@@ -0,0 +1,22 @@
+let closeBtn = document.querySelector('#btn');
+let sidebar = document.querySelector('.sidebar');
+let searchBtn = document.querySelector('.fa-search');
+
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+    menuBtnChange(); //calling the function(optional)
+});
+
+searchBtn.addEventListener("click", () => { // Sidebar open when you click on the search iocn
+    sidebar.classList.toggle("open");
+    menuBtnChange(); //calling the function(optional)
+});
+
+// following are the code to change sidebar button(optional)
+function menuBtnChange() {
+    if (sidebar.classList.contains("open")) {
+        closeBtn.classList.replace("fa-bars", "fa-times"); //replacing the iocns class
+    } else {
+        closeBtn.classList.replace("fa-times","fa-bars"); //replacing the iocns class
+    }
+}
